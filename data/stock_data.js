f6816a75e578a91929c16bfc913a4672b907f893
Ultimate-69 <170136337+Ultimate-69@users.noreply.github.com.>
2025-02-15 17:09:25 +0200

    added new stocks

--- data/stock_data.js
+++ data/stock_data.js
@@ -13,6 +13,34 @@ const stocksData = [
     trend: 'Neutral',
     id: 2,
 },
+{
+    name: 'Alex',
+    imgLink: 'https://cdn.discordapp.com/avatars/717446850625404960/0d0c066de1978b064534f3b19bc7374f.webp?size=1024&format=webp&width=640&height=640',
+    price: 10,
+    trend: 'Neutral',
+    id: 3,
+},
+{
+    name: 'Fallen',
+    imgLink: 'https://cdn.discordapp.com/avatars/713392201975070792/b2bc332e60bc4c94a546dc4b0d64c7af.webp?size=1024&format=webp&width=640&height=640',
+    price: 10,
+    trend: 'Neutral',
+    id: 4,
+},
+{
+    name: 'Superman',
+    imgLink: 'https://cdn.discordapp.com/avatars/1223451738531692686/09373eb17e3093a563c53fabf71961b0.webp?size=1024&format=webp&width=640&height=640',
+    price: 10,
+    trend: 'Neutral',
+    id: 5,
+},
+{
+    name: 'Behappy',
+    imgLink: 'https://cdn.discordapp.com/avatars/784138663901790248/ee076a5397ff24e4cca992b2f0285f32.webp?size=1024&format=webp&width=640&height=640',
+    price: 4,
+    trend: 'Decreasing',
+    id: 6,
+},
 ];
 
 export default stocksData;
