12d6bc06fb9bfdcaefdf4e5c383cc348d53b4228
Ultimate-69 <170136337+Ultimate-69@users.noreply.github.com.>
2025-02-15 15:07:47 +0200

    initial commit

---
+++ src/main.js
@@ -0,0 +1,7 @@
+const moneyText = document.querySelector("#money-text");
+let money = 0;
+moneyText.innerHTML = `Money: \$${money}`;
+
+const stocksText = document.querySelector("#stocks-text");
+let stocks = 0;
+stocksText.innerHTML = `Stocks: ${stocks}`;
